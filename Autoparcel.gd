8c162e6bb5e887271de635328c030d96079dedce
Styrr <st.d.koehler@gmx.de>
2020-08-21 15:30:43 +0200

    added first very simple level border by rocks

--- Autoparcel.gd
+++ Autoparcel.gd
@@ -62,46 +62,61 @@ func _ready():
 func _generate_world():
     var goldres = load("res://environment/Gold.tscn")
     var rockres = load("res://environment/Rock.tscn")
-    var W2 = floor(WIDTH/2.0)
-    var H2 = floor(HEIGHT/2.0)
-    for x in WIDTH:
-        for y in HEIGHT:
+    var BORDER = 20
+    var WB = WIDTH+2*BORDER
+    var HB = HEIGHT+2*BORDER
+    var W2 = floor(WB/2.0)
+    var H2 = floor(HB/2.0)
+    for x in WB:
+        for y in HB:
+            $GroundMap.set_cellv(Vector2(x-W2, y-H2), TILES.Dirt_Dark)
             var pos0 = Vector2(16,16)
-            var noise = dirt_patch_noise.get_noise_2d(float(x), float(y))
-            if noise > -0.2:
-                $GrassMap.set_cellv(Vector2(x-W2, y-H2), _get_tile_index(noise))
-                
-                var noiserock = randi()%300
-                if noiserock == 0:
-                    $Navigation2D/RockNavigationMap.set_cellv(Vector2(x-W2, y-H2), 1)
-                    var ri = rockres.instance()
-                    ri.initialize(pos0 + 32*Vector2(x-W2, y-H2),2)
-                    $YSort/RockContainer.add_child(ri)
-                elif noiserock < 5:
-                    $Navigation2D/RockNavigationMap.set_cellv(Vector2(x-W2, y-H2), 1)
-                    var ri = rockres.instance()
-                    ri.initialize(pos0 + 32*Vector2(x-W2, y-H2),0)
-                    $YSort/RockContainer.add_child(ri)
+            # only place the stuff within border range, otherwise just layer grass
+            if (x) > BORDER and (x) < (WB-BORDER) and (y) > BORDER and (y) < (HB-BORDER): 
+                var noise = dirt_patch_noise.get_noise_2d(float(x), float(y))
+                if noise > -0.2:
+                    $GrassMap.set_cellv(Vector2(x-W2, y-H2), _get_tile_index(noise))
+                    
+                    var noiserock = randi()%300
+                    if noiserock == 0:
+                        $Navigation2D/RockNavigationMap.set_cellv(Vector2(x-W2, y-H2), 1)
+                        var ri = rockres.instance()
+                        ri.initialize(pos0 + 32*Vector2(x-W2, y-H2),2)
+                        $YSort/RockContainer.add_child(ri)
+                    elif noiserock < 5:
+                        $Navigation2D/RockNavigationMap.set_cellv(Vector2(x-W2, y-H2), 1)
+                        var ri = rockres.instance()
+                        ri.initialize(pos0 + 32*Vector2(x-W2, y-H2),0)
+                        $YSort/RockContainer.add_child(ri)
+                        
+                    else:
+                        $Navigation2D/RockNavigationMap.set_cellv(Vector2(x-W2, y-H2), 0)
                     
                 else:
                     $Navigation2D/RockNavigationMap.set_cellv(Vector2(x-W2, y-H2), 0)
+                    noise = gold_noise.get_noise_2d(float(x), float(y))
+                    
+                    if noise >0.25:
+                        print("Gold!")
+                        var gi = goldres.instance()
+                        gi.initialize(pos0 + 32*Vector2(x-W2, y-H2),noise)
+                        $GoldContainer.add_child(gi)
+                        gi.connect("player_entered", $YSort/Player, "_on_gold_entered")
+                        gi.connect("player_exited", $YSort/Player, "_on_gold_exited")
+                        gi.connect("player_clicked", $YSort/Player, "_on_gold_clicked")
+
                 
             else:
-                $Navigation2D/RockNavigationMap.set_cellv(Vector2(x-W2, y-H2), 0)
-                noise = gold_noise.get_noise_2d(float(x), float(y))
-                
-                if noise >0.25:
-                    print("Gold!")
-                    var gi = goldres.instance()
-                    gi.initialize(pos0 + 32*Vector2(x-W2, y-H2),noise)
-                    $GoldContainer.add_child(gi)
-                    gi.connect("player_entered", $YSort/Player, "_on_gold_entered")
-                    gi.connect("player_exited", $YSort/Player, "_on_gold_exited")
-                    gi.connect("player_clicked", $YSort/Player, "_on_gold_clicked")
-                    
+                $GrassMap.set_cellv(Vector2(x-W2, y-H2), TILES.Grass_Dry)
             
-                
-            $GroundMap.set_cellv(Vector2(x-W2, y-H2), TILES.Dirt_Dark)
+            # create the border obstacles
+            if      ((x == BORDER or x == WB-BORDER) and (y >= BORDER and y <= HB-BORDER)) \
+                or  ((y == BORDER or y == HB-BORDER) and (x >= BORDER and x <= WB-BORDER)):
+                $Navigation2D/RockNavigationMap.set_cellv(Vector2(x-W2, y-H2), 1)
+                var ri = rockres.instance()
+                ri.initialize(pos0 + 32*Vector2(x-W2, y-H2),2)
+                $YSort/RockContainer.add_child(ri)
+            
             
     $GrassMap.update_bitmask_region()
     $GroundMap.update_bitmask_region()
